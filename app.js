47d37ad8222c74fc20f5c479d476ee66f46b074c
noorsheikh <noorsheikh.me@gmail.com>
2016-06-22 10:50:09 +0430

    Chatrooms created

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
 var rooms = require("./data/rooms.json");
+var bodyParser = require('body-parser');
+var uuid = require('node-uuid');
 
 app.set("views", "./views");
 app.set("view engine", "jade");
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
 	res.render("index", {
@@ -16,9 +19,30 @@ app.get('/', function(req, res){
 
 app.get('/admin/rooms', function(req, res){
 	res.render("rooms", {
-		title: "Chat Rooms",
+		title: "Admin Rooms",
 		rooms: rooms
 	});
+});
+
+app.get('/admin/rooms/add', function(req, res){
+	res.render("add");
+});
+
+app.post('/admin/rooms/add', function(req, res){
+	var room = {
+		name: req.body.name,
+		id: uuid.v4()
+	};
+	rooms.push(room);
+	res.redirect("/admin/rooms");
+});
+
+app.get("/admin/rooms/delete/:id", function(req, res) {
+	var roomId = req.params.id;
+
+	rooms = rooms.filter(r => r.id !== roomId)
+
+	res.redirect("/admin/rooms");
 });
 
 app.listen(3000, function(){
