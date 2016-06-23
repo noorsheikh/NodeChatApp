b07627e1826f6bc1016dbf53c695a58461964d4d
noorsheikh <noorsheikh.me@gmail.com>
2016-06-23 08:36:54 +0430

    CRUD for chatrooms

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var rooms = require("./data/rooms.json");
 var bodyParser = require('body-parser');
 var uuid = require('node-uuid');
+var _= require("lodash");
 
 app.set("views", "./views");
 app.set("view engine", "jade");
@@ -37,10 +38,36 @@ app.post('/admin/rooms/add', function(req, res){
 	res.redirect("/admin/rooms");
 });
 
+app.get("/admin/rooms/edit/:id", function(req, res) {
+	var roomId = req.params.id;
+
+	var room = _.find(rooms, r => r.id === roomId);
+	if(!room) {
+		res.sendStatus(404);
+		return;
+	}
+
+	res.render("edit", { room });
+});
+
+app.post('/admin/rooms/edit/:id', function(req, res){
+	var roomId = req.params.id;
+
+	var room = _.find(rooms, r => r.id === roomId);
+	if(!room) {
+		res.sendStatus(404);
+		return;
+	}
+
+	room.name = req.body.name;
+
+	res.redirect("/admin/rooms");
+});
+
 app.get("/admin/rooms/delete/:id", function(req, res) {
 	var roomId = req.params.id;
 
-	rooms = rooms.filter(r => r.id !== roomId)
+	rooms = rooms.filter(r => r.id !== roomId);
 
 	res.redirect("/admin/rooms");
 });
