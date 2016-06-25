6eb442488d74b94c0efcb1804561bdfc4f574375
noorsheikh <noorsheikh.me@gmail.com>
2016-06-25 15:20:35 +0430

    Another commit for admin section

--- admin.js
+++ admin.js
@@ -26,7 +26,7 @@ router.route('/rooms/add')
 	});
 
 router.route('/rooms/edit/:id')
-	.get(function(req, res) {
+	.all(function(req, res, next) {
 		var roomId = req.params.id;
 
 		var room = _.find(rooms, r => r.id === roomId);
@@ -34,19 +34,17 @@ router.route('/rooms/edit/:id')
 			res.sendStatus(404);
 			return;
 		}
+		res.locals.room = room;
+		next()
+	})
+	.get(function(req, res) {
+		
 
-		res.render("edit", { room });
+		res.render("edit");
 	})
 	.post(function(req, res){
-		var roomId = req.params.id;
 
-		var room = _.find(rooms, r => r.id === roomId);
-		if(!room) {
-			res.sendStatus(404);
-			return;
-		}
-
-		room.name = req.body.name;
+		res.locals.room.name = req.body.name;
 
 		res.redirect(req.baseUrl + "/rooms");
 	});
