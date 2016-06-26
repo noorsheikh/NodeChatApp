ac3200f3d3e2ddac4662b0c6ebeb12aae08a1e96
noorsheikh <noorsheikh.me@gmail.com>
2016-06-26 12:37:27 +0430

    Messaging section added

---
+++ api.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+var uuid = require('node-uuid');
+var rooms = require("./data/rooms.json");
+var messages = require("./data/messages.json");
+module.exports = router;
+
+router.get("/rooms", function(req, res){
+	res.json(rooms);
+});
+
+router.route("/rooms/:roomId/messages")
+	.get(function(req, res){
+		var roomId = req.params.roomId;
+		var roomMessages = messages
+			.filter(m => m.roomId == roomId);
+
+		var room = _.find(rooms, r => r.id === roomId);
+		if(!room) {
+			res.sendStatus(404);
+			return;
+		}
+		res.json({
+			room: room,
+			messages: roomMessages
+		})
+	})
+	.post(function(req, res){
+		var roomId = req.params.roomId;
+
+		var message = {
+			roomId: roomId,
+			text: req.body.text,
+			userId: "44f885e8-87e9-4911-973c-4074188f408a",
+			id: uuid.v4()
+		};
+
+		messages.push(message);
+
+		res.sendStatus(200);
+
+	})
+	.delete(function(req, res){
+		var roomId = req.params.roomId;
+
+		messages = messages.filter(m => m.roomdId !== roomId);
+
+		res.sendStatus(200);
+
+	});
--- app.js
+++ app.js
@@ -7,16 +7,21 @@ app.set("view engine", "jade");
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(express.static('node_modules/jquery/dist'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
-	res.render("index", {
+	res.render("home", {
 		title: "Home Page"
 	});
 });
 
 var adminRouter = require('./admin');
 app.use('/admin', adminRouter);
+
+var apiRouter = require('./api');
+app.use('/api', apiRouter);
 
 app.listen(3000, function(){
 	console.log('Chat app listening on port 3000!');
